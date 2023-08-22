3bf6eb50a70fea1be1cba9e5d7c547cc4513b1b1
mccoyp <mcpatino@microsoft.com>
2023-08-22 04:19:31 -0700

    Use MI client ID

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
    print("Request for index page received")
    container = "https://mcpatinostorage.blob.core.windows.net/backup"
-   credential = ManagedIdentityCredential()
+   credential = ManagedIdentityCredential(client_id="66adbee8-0d3d-4c93-955a-16de92b850c6")
    client = KeyVaultBackupClient("https://mcpatinokvhsm.managedhsm.azure.net", credential)
    backup_poller = client.begin_backup(container, use_managed_identity=True)
    return backup_poller.result()
