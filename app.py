6e0508620741522bd47e58b0eafc3f348137a3bc
mccoyp <mcpatino@microsoft.com>
2023-08-21 19:25:46 -0700

    Clean up

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-   print('Request for index page received')
-   return render_template('index.html')
+   print("Request for index page received")
+   return render_template("index.html")
 
-@app.route('/favicon.ico')
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, "static"),
+                               "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
-@app.route('/hello', methods=['POST'])
+@app.route("/hello", methods=["POST"])
 def hello():
-   name = request.form.get('name')
+   name = request.form.get("name")
 
    if name:
-       print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       print("Request for hello page received with name=%s" % name)
+       return render_template("hello.html", name = name)
    else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
+       print("Request for hello page received with no name or blank name -- redirecting")
+       return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
    app.run()
