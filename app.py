aa126067fec45e7c64d7a57e75b7fa30e6613ba5
mccoyp <mcpatino@microsoft.com>
2023-08-22 16:42:03 -0700

    Remove template reference

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
    print("Request for index page received")
-   return render_template("index.html")
+   return "hiiii :3"
 
 
 if __name__ == "__main__":
